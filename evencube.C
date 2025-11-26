e17d795f0f98614db664a2dc5fa8d71a17654212
monikaroj0502 <monikaroj0602@gmail.com>
2025-11-26 14:41:19 +0530

    Add files via upload

---
+++ evencube.C
@@ -0,0 +1,18 @@
+ #include <stdio.h>
+ int main()
+ {
+ 	int n ;
+ 	printf ("enter number");
+ 	scanf("%d",&n);
+ 	if (n%2==0)
+ 	{
+ 	printf("number is even \n cube of number =%d",n*n*n);
+	 	
+	}
+ 	else 
+	 {
+	 	printf("number is odd \n square of number =%d",n*n);
+	 	
+	 }
+ 	
+ }
