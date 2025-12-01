12777a2f28bba08f1cf0c8280c72142e8ddd0609
monikaroj0502 <monikaroj0602@gmail.com>
2025-12-01 11:20:26 +0530

    Add files via upload

---
+++ loop2.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,i,s;
+	printf("enter two numbers");
+	scanf("%d%d",&a,&b);
+	 
+	  for(i=1; i<=b; i++)
+	   {
+	   	s=i*i;
+	   	 
+	   	 printf("\nsquare=%d",s);
+	   }
+}
